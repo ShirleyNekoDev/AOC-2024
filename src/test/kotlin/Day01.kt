81038f592d8bc10db4d9e6842b974b583c96a115
Maximilian Stiede <maximilian.stiede@outlook.com>
2024-12-01 07:46:58 +0100

    solve day 1

--- src/test/kotlin/Day01.kt
+++ src/test/kotlin/Day01.kt
@@ -2,9 +2,18 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 object Day01Tests {
+    val SHORT = """
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
+    """.trimIndent().split('\n')
+
     @Test
     fun `Part 1 short`() {
-        assertEquals(1, Day01.part1(readInput("Day01_short")))
+        assertEquals(11, Day01.part1(SHORT))
     }
 
     @Test
@@ -14,7 +23,7 @@ object Day01Tests {
 
     @Test
     fun `Part 2 short`() {
-        assertEquals(1, Day01.part2(readInput("Day01_short")))
+        assertEquals(31, Day01.part2(SHORT))
     }
 
     @Test
