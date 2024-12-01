bbf0e31ea51778522d68c0c571981371d5af7f48
Maximilian Stiede <maximilian.stiede@outlook.com>
2024-12-01 07:46:58 +0100

    solve day 1

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,9 +1,44 @@
-object Day01 {
-    fun part1(input: List<String>): Int {
-        return input.toList().size
-    }
+import kotlin.collections.component1
+import kotlin.collections.component2
+import kotlin.math.absoluteValue
 
-    fun part2(input: List<String>): Int {
-        return input.toList().size
-    }
+object Day01 {
+    fun List<String>.parseLists() = this
+        .map { line -> line
+            .split(Regex("\\s+"))
+            .map { it.toInt() }
+            .apply { assert(size == 2) }
+        }
+        .fold(ArrayList<Int>() to ArrayList<Int>()) { acc, (left, right) ->
+            val (leftList, rightList) = acc
+            leftList.add(left)
+            rightList.add(right)
+            acc
+        }
+
+    fun part1(input: List<String>): Int = input.parseLists()
+        .also { (leftList, rightList) ->
+            leftList.sort()
+            rightList.sort()
+        }
+        .let { (leftList, rightList) ->
+            leftList.mapIndexed { index, left ->
+                val right = rightList[index]
+                (left - right).absoluteValue
+            }
+        }
+        .sum()
+
+    fun part2(input: List<String>): Int = input.parseLists()
+        .let { (leftList, rightList) ->
+            leftList.sorted() to
+            rightList.groupingBy { it }.eachCount()
+        }
+        .let { (leftList, rightMap) ->
+            leftList.mapIndexed { index, left ->
+                val rightCount = rightMap.getOrDefault(left, 0)
+                left * rightCount
+            }
+        }
+        .sum()
 }
--- src/test/kotlin/Day01.kt
+++ src/test/kotlin/Day01.kt
@@ -4,7 +4,7 @@ import kotlin.test.assertEquals
 object Day01Tests {
     @Test
     fun `Part 1 short`() {
-        assertEquals(1, Day01.part1(readInput("Day01_short")))
+        assertEquals(11, Day01.part1(readInput("Day01_short")))
     }
 
     @Test
@@ -14,7 +14,7 @@ object Day01Tests {
 
     @Test
     fun `Part 2 short`() {
-        assertEquals(1, Day01.part2(readInput("Day01_short")))
+        assertEquals(31, Day01.part2(readInput("Day01_short")))
     }
 
     @Test
